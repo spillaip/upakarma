4c9d373669412c0195ae2913de760f044ae11a21
SURESH PILLAIPAKKAM BAGUKKUDUMBI <spillaip@gmail.com>
2020-07-28 09:42:46 +0530

    Add files via upload
    Initial commit

---
+++ main/java/com/vaishnavismeclass/upakarma/tamil2018/MainActivity.java
@@ -0,0 +1,18 @@
+package com.vaishnavismeclass.upakarma.tamil2018;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instanciating an array list (you don't need to do this,
+        // you already have yours).
+
+
+    }
+}
